127b28ab63eac0c9c8477fadcb0c8b6944fac515
wangchengjian <wangchengjian@DESKTOP-8F5T9E0>
2023-05-24 18:09:17 +0800

    json文件获取方式调整

--- OcelotDemo/Program.cs
+++ OcelotDemo/Program.cs
@@ -1,26 +1,30 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
-var builder = WebApplication.CreateBuilder(args)
-    ;
+var builder = WebApplication.CreateBuilder(args);
 
-builder.Host
-.ConfigureAppConfiguration((hostingContext, config) =>
-{
-    config.AddJsonFile($"Ocelot.json", optional: false, reloadOnChange: true);
-})
-;
+//builder.Host
+//.ConfigureAppConfiguration((hostingContext, config) =>
+//{
+//    config.AddJsonFile($"Ocelot.json", optional: false, reloadOnChange: true);
+//    //config.AddOcelot((global::Microsoft.AspNetCore.Hosting.IWebHostEnvironment)hostingContext.HostingEnvironment); //12.x版本 要加环境  自动查询所有Ocelot.xx.json
+//})
+//;
 
 
 // Add services to the container.
-
+builder.Services.AddOcelot();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddOcelot();
 
-builder.WebHost.UseUrls("http://*:1000");
+builder.WebHost.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config
+        .AddOcelot((global::Microsoft.AspNetCore.Hosting.IWebHostEnvironment)hostingContext.HostingEnvironment);
+}).UseUrls("http://*:1000");
+//builder.WebHost.UseUrls("http://*:1000");
 var app = builder.Build();
 app.UseOcelot().Wait();
 // Configure the HTTP request pipeline.
